dbbf0b3409bf9ac1962a21b160877435eb565db3
ywq1985124 <ywq1985124@yahoo.com.cn>
2014-11-20 11:05:21 +0800

    init
    init

---
+++ BaseObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseObject
+//
+//  Created by 杨伟清 on 14/11/20.
+//  Copyright (c) 2014年 绿点科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseObject
+//
+//  Created by 杨伟清 on 14/11/20.
+//  Copyright (c) 2014年 绿点科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
